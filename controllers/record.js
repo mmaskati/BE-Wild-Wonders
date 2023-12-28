2cf27d2e90979af79e1fad318d07e1f91b6e700c
salma45 <salmaa-125@outlook.com>
2023-12-28 15:42:18 +0300

    record model,routes and controllers

---
+++ controllers/record.js
@@ -0,0 +1,11 @@
+//API's Function 
+
+const {Record} = require("../models/Record");
+
+
+// CRUD operations
+//HTTP POST- Create - Post the data 
+// HTTP GET - Read - Retrieves the data
+// HTTP PUT - update - update the data
+//HTTP Delete/GET/POST - delete- delete the data
+
---
+++ routes/record.js
@@ -0,0 +1,18 @@
+const express= require('express');
+// const methodOverride= require('method-override')
+const router= express.Router();
+router.use(express.urlencoded({extended: true}));
+const RecordCntrl= require("../controllers/record");
+const isLoggedIn = require('../helper/isLoggedIn')
+
+
+//Routes
+router.get("/add", RecordCntrl.record_create_get);
+router.post("/add", RecordCntrl.record_create_post);
+router.get("/index", RecordCntrl.record_index_get);
+router.get("/detail", RecordCntrl.record_show_get);
+router.get("/delete", RecordCntrl.record_delete_get);
+router.get("/edit", RecordCntrl.record_edit_get);
+router.post("/update", RecordCntrl.record_update_post);
+
+
